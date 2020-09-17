f96dc704e35e154b6bb55c9f36b6c94deb3b40af
tangkaiqiang <justforgit@163.com>
2020-09-17 21:34:59 +0800

    render process index.html

--- src/renderProcess/handler.js
+++ src/renderProcess/handler.js
@@ -1,6 +1,7 @@
 const { ipcRenderer } = require('electron');
 const startBtn = document.querySelector('#start-btn');
 const linkInput = document.querySelector('#link');
+const collectBtn = document.querySelector('#collect-btn');
 
 ipcRenderer.on('crawl-response', (e, res) => {
   console.log(res);
@@ -11,4 +12,8 @@ startBtn.addEventListener('click', (e) => {
   const url =
     'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98&productId=50647371545&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1';
   ipcRenderer.send('start-crawl', url);
+});
+
+collectBtn.addEventListener('click', (event) => {
+  $('#form-modal').show();
 });
