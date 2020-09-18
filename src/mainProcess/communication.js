a8fd2038e751e9fe61677602a15424a32115c6ed
tangkaiqiang <justforgit@163.com>
2020-09-18 16:40:15 +0800

    change output

--- src/mainProcess/communication.js
+++ src/mainProcess/communication.js
@@ -1,6 +1,13 @@
-const { ipcMain } = require('electron');
+const { ipcMain, dialog } = require('electron');
 const crawl = require('./crawl');
 
 ipcMain.on('start-crawl', (event, args) => {
   crawl(event, args);
+});
+
+ipcMain.on('choose-output', (event, args) => {
+  const result = dialog.showOpenDialogSync({ properties: ['openDirectory'] });
+  if (result) {
+    event.reply('output-selected', result[0]);
+  }
 });
