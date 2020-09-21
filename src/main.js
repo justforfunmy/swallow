a0c51ba9234ebcdc398a5ac24e95e2b607f2688b
tangkaiqiang <justforgit@163.com>
2020-09-21 11:31:45 +0800

    test app

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ function createWindow() {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
   // mainWindow.maximize();
 }
 
--- test/spec.js
+++ test/spec.js
@@ -1,48 +1,107 @@
-// const Application = require('spectron').Application;
-// const assert = require('assert');
-// const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
-// const path = require('path');
-const { spawn } = require('child_process');
-const puppeteer = require('puppeteer');
-const electron = require('electron');
-const { assert } = require('console');
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
+const path = require('path');
 
-describe('Application launch', async function () {
-  // this.timeout(10000);
+describe('Application launch', function () {
+  this.timeout(10000);
 
-  // const app = new Application({
-  //   path: electronPath,
-  //   args: [path.resolve(__dirname, '..')]
-  // });
+  const app = new Application({
+    // Your electron path can be any binary
+    // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+    // But for the sake of the example we fetch it from our node_modules.
+    path: electronPath,
 
-  // beforeEach(async (done) => {
-  //   await app.start();
-  //   done();
-  // });
+    // Assuming you have the following directory structure
 
-  // afterEach(async (done) => {
-  //   if (app && app.isRunning()) {
-  //     await app.stop();
-  //     done();
-  //   }
-  // });
+    //  |__ my project
+    //     |__ ...
+    //     |__ main.js
+    //     |__ package.json
+    //     |__ index.html
+    //     |__ ...
+    //     |__ test
+    //        |__ spec.js  <- You are here! ~ Well you should be.
 
-  // it('shows an initial window', async function (done) {
-  //   await app.client.waitUntilWindowLoaded();
-  //   return app.client.getWindowCount().then(function (count) {
-  //     assert.equal(count, 1);
-  //     // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-  //     // assert.equal(count, 2)
-  //     done();
-  //   });
-  // });
+    // The following line tells spectron to look and use the main.js file
+    // and the package.json located 1 level above.
+    args: [path.join(__dirname, '..')]
+  });
 
-  const spwanProcess = spawn(electron, ['.', '--remote-debugging-port=9200'], { shell: true });
-  const app = await puppeteer.connect({ browserURL: 'http://localhost:9200' });
-  const [page] = await app.pages();
+  beforeEach(function () {
+    return app.start();
+  });
 
-  it('start button', async () => {
-    const startBtn = page.$('.start-btn');
-    assert.equal(startBtn.innerText, '开始');
+  afterEach(function () {
+    if (app && app.isRunning()) {
+      return app.stop();
+    }
+  });
+
+  it.skip('shows an initial window', async () => {
+    const count = await app.client.getWindowCount();
+    return assert.strictEqual(count, 1);
+  });
+
+  it.skip('has the correct title', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const title = await app.browserWindow.getTitle();
+    return assert.strictEqual(title, 'Swallow');
+  });
+
+  it.skip('does not have the dev tools open', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const devToolsOpen = await app.browserWindow.isDevToolsOpened();
+    return assert.strictEqual(devToolsOpen, false);
+  });
+
+  it.skip('has a button with the text "添加路径"', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const buttonText = await (await app.client.$('#next-link')).getText();
+    return assert.strictEqual(buttonText, '添加路径');
+  });
+
+  it.skip('should have only one link input when the application start', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const linkInput = await app.client.$$('input[name="link"]');
+    return assert.strictEqual(linkInput.length, 1);
+  });
+
+  it.skip('should have at least two link input when the "添加路径" button pressed', async () => {
+    await app.client.waitUntilWindowLoaded();
+    await (await app.client.$('#next-link')).click();
+    const linkInput = await app.client.$$('input[name="link"]');
+    return assert.strictEqual(linkInput.length, 2);
+  });
+
+  it.skip('should succefully remove a link input', async () => {
+    await app.client.waitUntilWindowLoaded();
+    await (await app.client.$('#next-link')).click();
+    await (await app.client.$('#delete-link')).click();
+    const linkInput = await app.client.$$('input[name="link"]');
+    return assert.strictEqual(linkInput.length, 1);
+  });
+
+  it.skip('should modal display none when the application start', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const modal = await app.client.$('#form-modal');
+    const display = await modal.getCSSProperty('display');
+    return assert.strictEqual(display.value, 'none');
+  });
+
+  it.skip('should open a modal when the "添加采集数据项" buttons pressed', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const collectButtons = await app.client.$$('.collect-btn');
+    await collectButtons[0].click();
+    const modal = await app.client.$('#form-modal');
+    const display = await modal.getCSSProperty('display');
+    return assert.strictEqual(display.value, 'block');
+  });
+
+  it('should add properties succefully', async () => {
+    await app.client.waitUntilWindowLoaded();
+    const form = await app.client.$$('.form-instance');
+    await (await form[0].$('.collect-btn')).click();
+    const modal = await app.client.$('#form-modal');
   });
 });
